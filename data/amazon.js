c9eba45adea6f32c22b5aac160463bbb8c9810dc
Chris McAvoy <chris.mcavoy@gmail.com>
2013-11-14 19:45:07 -0600

    get the isbn for the page you're on from the canonical link

--- data/amazon.js
+++ data/amazon.js
@@ -1,14 +1,26 @@
+function getIsbnCanonical() {
+  // get all the links, find canonical
+  var links = document.getElementsByTagName('link');
+  for (var i=0; i<links.length; i++) {
+    if (links[i].rel == 'canonical') {
+      var isbnMatch = links[i].href.match(/dp\/(.*)$/);
+      if (isbnMatch && isbnMatch.length > 1) {
+        return isbnMatch[1];
+      };
+    }
+  }
+  return false;
+};
 
-
-var isbnEl = document.getElementById('ASIN');
-if (isbnEl) {
-  self.port.emit('gotIsbn', isbnEl.value);
+var isbn = getIsbnCanonical();
+if (isbn) {
+  self.port.emit('gotIsbn', isbn);
 };
 
 self.port.on('gotLinkToCpl', function(cplLink) {
   var libLink = document.createElement('a');
   libLink.href = cplLink;
   libLink.innerHTML = "CPL Reserve";
-  var optBox = document.getElementById('tmmSwatches');
+  var optBox = document.getElementById('tmmSwatches') || document.getElementsByClassName('twisterMediaMatrix')[0];
   optBox.appendChild(libLink);
 });
